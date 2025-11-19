cb507f379489a02fa747e6f47968fa77c5a17f9e
sudodot <haikhalroy@gmail.com>
2025-11-19 08:16:39 +0700

    chore: passed schema directly to auth.ts and removed propietary neon driver from drizzle

--- drizzle/db.ts
+++ drizzle/db.ts
@@ -1,15 +1,6 @@
 // if using local postgres instance
-// import { drizzle } from "drizzle-orm/node-postgres";
+import { drizzle } from "drizzle-orm/node-postgres";
 
-// if using neon serverless
-import { drizzle } from "drizzle-orm/neon-http";
-// if using neon serverless and need a synchronous connection
-import { neon } from "@neondatabase/serverless";
-import * as schema from "./schema";
-
-const sql = neon(process.env.DATABASE_URL!);
-export const db = drizzle({ client: sql, schema });
-
-// export const db = drizzle(process.env.DATABASE_URL!);
+export const db = drizzle(process.env.DATABASE_URL!);
 
 // refer to: https://orm.drizzle.team/docs/get-started/neon-new
--- lib/auth.ts
+++ lib/auth.ts
@@ -2,10 +2,14 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/drizzle/db";
 import { admin } from "better-auth/plugins";
+import * as schema from "../drizzle/schema";
 
 export const auth = betterAuth({
     database: drizzleAdapter(db, {
         provider: "pg",
+        schema: {
+            ...schema,
+        },
     }),
     emailAndPassword: {
         enabled: true,
